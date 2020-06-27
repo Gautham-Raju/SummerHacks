ee9045ef83eda6c1bf5a93db9da8dcae127e7090
rskakarlapudi <rajukakarlapudi@macbook-pro-6.home>
2020-06-27 18:54:22 -0500

    group-user integration

--- Attempt1/trials/users/models.py
+++ Attempt1/trials/users/models.py
@@ -2,19 +2,36 @@ from django.db import models
 
 # Create your models here.
 class UserData(models.Model):
-    #profilePic = 
+    #userID =  models.BigIntegerField()
+    #picture = models.ImageField()
     name = models.CharField(max_length = 100)
-    #email =
+    #email = models.EmailField(max_length = 254)
     location = models.CharField(max_length = 100)
     school = models.CharField(max_length = 100)
     year = models.BigIntegerField()
     biography = models.CharField(max_length = 350)
+    club_membership = [1, 2]
 
     #notifications
-    announcements = models.BooleanField()
-    election_start = models.BooleanField()
-    election_creation = models.BooleanField()
-    election_end = models.BooleanField()
-    communication_message = models.BooleanField()
-    communicatoin_group = models.BooleanField()
-    calendar = models.BooleanField()
+    announcements = models.BooleanField(null = True)
+    election_start = models.BooleanField(null = True)
+    election_creation = models.BooleanField(null = True)
+    election_end = models.BooleanField(null = True)
+    communication_message = models.BooleanField(null = True)
+    communication_group = models.BooleanField(null = True)
+    calendar = models.BooleanField(null = True)
+
+class ClubData(models.Model):
+    #clubName = models.CharField(max_length = 100)
+    #clubID = models.BigIntegerField()
+    #picture = models.ImageField()
+    #color = models.
+    president = models.CharField(max_length = 100)
+    vpresident = models.CharField(max_length = 100)
+    treasurer = models.CharField(max_length = 100)
+    secretary = models.CharField(max_length = 100)
+
+class UserClub(models.Model):
+    user = models.ForeignKey('UserData', on_delete=models.CASCADE)
+    club = models.ForeignKey('ClubData', on_delete=models.CASCADE)
+    isAdmin = models.BooleanField(null = True)
